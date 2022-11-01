8266fc08abcdc0565d3896f6c4fc28cbdc56034e
IreneQuero <queroirene@gmail.com>
2022-11-01 18:48:17 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const excuse = document.querySelector("#the-excuse");
+  const btn = document.querySelector("#btn");
+  let generateExcuse = () => {
+    let pronoun = ["A", "The"];
+    let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pincone"];
+    let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+    let possetion = ["homework", "toe", "car", "shoe"];
+    let where = ["on the street", "in my house", "in my driveway"];
+
+    let proIndx = Math.floor(Math.random() * pronoun.length);
+    let subjIndx = Math.floor(Math.random() * subject.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let possetionIndex = Math.floor(Math.random() * possetion.length);
+    let whereIndex = Math.floor(Math.random() * where.length);
+    let result =
+      pronoun[proIndx] +
+      " " +
+      subject[subjIndx] +
+      " " +
+      action[actionIndex] +
+      " " +
+      possetion[possetionIndex] +
+      " " +
+      where[whereIndex];
+    console.log(result);
+    return result;
+  };
+  btn.addEventListener("click", () => {
+    excuse.innerHTML = generateExcuse();
+  });
+  excuse.innerHTML = generateExcuse();
 };
